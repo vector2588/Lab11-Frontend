e362c5fa8872ae38be2a58857bf30281ff8bbbd2
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-22 22:10:45 +0700

    initial commit

---
+++ src/services/EventService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_BACKEND_URL,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
+  saveEvent(event) {
+    return apiClient.post('/events', event)
+  },
+  getEventByKeyword(keyword, perPage, page) {
+    return apiClient.get(
+      'events?_limit=' + perPage + '&_page=' + page + '&title=' + keyword
+    )
+  }
+}
