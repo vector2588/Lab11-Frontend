25db78fcb01fcbfc4087c0346dad46701c64a7fe
Vector2588 <natkamol082@gmail.com>
2021-09-24 18:57:40 +0700

    2.3 Add event with image

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -20,6 +20,8 @@
         v-model="event.organizer.id"
         label="Select an Organizer"
       />
+      <h3>The image of the Event</h3>
+      <UploadImages @changed="handleImages" />
       <button type="submit">Submit</button>
     </form>
 
@@ -28,9 +30,12 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
-
+import UploadImages from 'vue-upload-drop-images'
 export default {
   inject: ['GStore'],
+  components: {
+    UploadImages
+  },
 
   data() {
     return {
@@ -61,6 +66,9 @@ export default {
         .catch(() => {
           this.$router.push('NetworkError')
         })
+    },
+    handleImages(files) {
+      console.log(files);
     }
   }
 }
